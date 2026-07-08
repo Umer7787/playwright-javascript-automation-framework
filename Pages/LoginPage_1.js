08c2973d044358f4a6d0a844343a740660bde1ee
Umer7787 <shaikhumer.in@gmail.com>
2026-07-09 02:14:38 +0530

    Task_02_Completed...

--- Pages/LoginPage_1.js
+++ Pages/LoginPage_1.js
@@ -8,6 +8,7 @@ class LoginPage_1{
         this.InputUsername = '#user-name';
         this.InputPassword = '#password';
         this.LoginBtn = '#login-button';
+        this.ErrorMassege = "h3[data-test='error']"
     }
 
     async gotoLoginPage(){
@@ -18,7 +19,12 @@ class LoginPage_1{
         await this.page.locator(this.InputUsername).fill(username);
         await this.page.locator(this.InputPassword).fill(password);
         await this.page.locator(this.LoginBtn).click();
-        await expect(this.page).toHaveURL('https://www.saucedemo.com/inventory.html')
 
+    }
+
+
+    async VerifyErrorMassege(){
+        
+        await expect(this.page.locator(this.ErrorMassege)).toHaveText('Epic sadface: Username and password do not match any user in this service')
     }
 }
--- tests/Task_01_Login/Login.spec.js
+++ tests/Task_01_Login/Login.spec.js
@@ -7,6 +7,20 @@ test('Verify Login Functionality', async({page})=>{
 
     await login.gotoLoginPage();
     await login.login('standard_user','secret_sauce')
-    await page.waitForTimeout(3000)
+    await page.waitForTimeout(1000)
+
+    await expect(this.page).toHaveURL('https://www.saucedemo.com/inventory.html')
+
+    
+})
+
+test.only('Login with Inavlid Credentials', async({page})=>{
+
+    const Login = new LoginPage_1(page)
+
+    await Login.gotoLoginPage();
+    await Login.login('Umer','1234')
+    await page.waitForTimeout(1000)
+    await Login.VerifyErrorMassege()
     
 })
